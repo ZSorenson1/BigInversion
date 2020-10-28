cced94bc07161c016f90ac04bcf466dd926a899e
Zachary Sorenson <zacharyjsorenson@gmail.com>
2020-10-28 16:26:45 -0400

    update for putting it together assignment

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -2,14 +2,20 @@ import React, { Component } from 'react';
 import { findRenderedDOMComponentWithClass } from 'react-dom/test-utils';
 
 class PersonCard extends Component {
-    
+    constructor(props) {
+        super(props);
+        this.state = {
+            age: parseInt(props.age)
+        } 
+    };
     render () {
-        const { firstName, lastName, age, hairColor } = this.props;
+        var { firstName, lastName, age, hairColor } = this.props
         return (
             <div>
                 <h2>{lastName}, {firstName}</h2>
-                <p>Age: {age} </p>
+                <p>Age: {this.state.age} </p>
                 <p>Hair Color: {hairColor}</p>
+                <button onClick={ () => this.setState({age: this.state.age+1})}>Birthday Button for {firstName}</button>
             </div>
         );
     }
