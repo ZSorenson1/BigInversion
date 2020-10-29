e1bc291502347b637fae0221ec25e8d807724f92
Zachary Sorenson <zacharyjsorenson@gmail.com>
2020-10-29 14:31:19 -0400

    first commit

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,24 +1,15 @@
-import React, { Component } from 'react';
-import { findRenderedDOMComponentWithClass } from 'react-dom/test-utils';
+import React from 'react';
 
-class PersonCard extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            age: parseInt(props.age)
-        } 
-    };
-    render () {
-        var { firstName, lastName, age, hairColor } = this.props
-        return (
-            <div>
-                <h2>{lastName}, {firstName}</h2>
-                <p>Age: {this.state.age} </p>
-                <p>Hair Color: {hairColor}</p>
-                <button onClick={ () => this.setState({age: this.state.age+1})}>Birthday Button for {firstName}</button>
-            </div>
-        );
-    }
+
+
+const PersonCard = props => {
+    return(
+        <div>
+            <h1>{ props.lastName }, { props.firstName }</h1>
+            <p>Age: { props.age }</p>
+            <p>Hair Color: { props.hairColor }</p>
+        </div>
+    );
 }
 
 export default PersonCard;
